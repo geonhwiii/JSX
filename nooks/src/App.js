41753425ea0efae0026e0298ad66ee24790a65e3
geonhwiii <g01063962671@gmail.com>
2019-07-12 00:20:54 +0900

    useInput complete

--- nooks/src/App.js
+++ nooks/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { useInput } from './components/useInput';
 
-function App() {
+const App = () => {
+  const maxLen = value => !value.includes('@');
+  const name = useInput("Mr.", maxLen)
   return (
     <>
-      
+      <h1>Hello</h1>
+      <input type="text" placeholder="Name" {...name}/>
     </>
   );
 }
--- nooks/src/components/useInput.jsx
+++ nooks/src/components/useInput.jsx
@@ -1,5 +1,16 @@
 import { useState } from 'react';
 
-export const useInput = (initialValue) => {
-  const [value, setValue] = useState(initialValue)
-}
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = e => {
+    const { target: { value } } = e;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
